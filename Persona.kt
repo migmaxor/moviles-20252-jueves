90b10135a436b54249d8b51c94c78fb0276344ef
Miguel Ángel Askar Rodríguez <miguel@MacBook-Air-de-Miguel.local>
2025-09-04 09:28:51 -0500

    Se crea el método imprimirNombre

--- Persona.kt
+++ Persona.kt
@@ -3,4 +3,8 @@ class Persona(var nombre: String, var edad: Int) {
     fun ImprimirEdad (){
         println("Edad: $edad")
     }
+
+    fun imprimirNombre() {
+        println("Nombre: $nombre")
+    }
 }
